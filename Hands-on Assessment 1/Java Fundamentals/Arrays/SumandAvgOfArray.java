0972f930c6666e4e793a1ffc670d6264207400e6
PriyaKichaa <122371093+PriyaKichaa@users.noreply.github.com>
2024-07-04 14:19:51 +0530

    Create SumandAvgOfArray.java

---
+++ Hands-on Assessment 1/Java Fundamentals/Arrays/SumandAvgOfArray.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class SumandAvgOfArray {
+
+    static int sum(int[] arr) {
+        int sum = 0;
+        for (int i : arr)
+            sum += i;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        int sum = sum(arr);
+        System.out.println("Sum: "+sum);
+        int avg = sum / n;
+        System.out.println("Average: "+avg);
+    }
+}
