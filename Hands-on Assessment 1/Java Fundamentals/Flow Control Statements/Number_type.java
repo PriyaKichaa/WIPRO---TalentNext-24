81e4fa379f51b3a4753289056d8e6556303d143b
PriyaKichaa <122371093+PriyaKichaa@users.noreply.github.com>
2024-07-04 14:19:01 +0530

    Create Number_type.java

---
+++ Hands-on Assessment 1/Java Fundamentals/Flow Control Statements/Number_type.java
@@ -0,0 +1,13 @@
+import java.util.*; 
+public class Number_type { 
+	public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in); 
+    int n=sc.nextInt(); 
+    if(n==0) 
+      System.out.println("Zero"); 
+		else if(n>0) 
+			System.out.println("Positive"); 
+		else 
+			System.out.println("Negative"); 
+	} 
+}
