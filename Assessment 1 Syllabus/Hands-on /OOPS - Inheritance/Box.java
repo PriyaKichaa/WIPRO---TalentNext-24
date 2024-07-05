51e1bcd453881a1b9b3e29c5a2ad52125ea241fa
Priya Kicha <122371093+PriyaKichaa@users.noreply.github.com>
2024-07-05 15:25:59 +0530

    commit

--- Assessment 1 Syllabus/Hands-on /OOPS - Inheritance/Box.java
+++ Assessment 1 Syllabus/Hands-on /OOPS - Inheritance/Box.java
@@ -1,1 +1,23 @@
-
+public class Box{
+    int width;
+    int depth;
+    int height;
+    Box(int width, int depth, int height){
+        this.width = width;
+        this.depth = depth;
+        this.height = height;
+    }
+    public int volume(){
+        return width * depth * height;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter box dimensions: ");
+        int width = sc.nextInt();
+        int depth = sc.nextInt();
+        int height = sc.nextInt();
+        
+        Box box = new Box(width, depth, height);
+        System.out.println("Volume of the box is: " + box.volume());
+    }
+}
