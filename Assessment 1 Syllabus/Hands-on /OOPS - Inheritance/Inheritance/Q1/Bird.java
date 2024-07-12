97125ca1e3ed9e50b22cece864640d4d72c510d6
Priya Kicha <122371093+PriyaKichaa@users.noreply.github.com>
2024-07-12 14:13:35 +0530

    finished inheritance

--- Assessment 1 Syllabus/Hands-on /OOPS - Inheritance/Inheritance/Q1/Bird.java
+++ Assessment 1 Syllabus/Hands-on /OOPS - Inheritance/Inheritance/Q1/Bird.java
@@ -1,8 +1,9 @@
 public class Bird extends Animal {
+    @Override
     public void eat() {
         System.out.println("Bird is eating");
     }
-
+    @Override
     public void sleep() {
         System.out.println("Bird is sleeping");
     }
