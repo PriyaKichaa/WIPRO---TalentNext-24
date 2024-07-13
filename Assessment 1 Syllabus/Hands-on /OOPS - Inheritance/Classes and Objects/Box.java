7f32cfd953295632757c7512475fe9b5e62bcd8d
Priya Kicha <122371093+PriyaKichaa@users.noreply.github.com>
2024-07-13 20:47:21 +0530

    mod 1 done

--- Assessment 1 Syllabus/Hands-on /OOPS - Inheritance/Classes and Objects/Box.java
+++ Assessment 1 Syllabus/Hands-on /OOPS - Inheritance/Classes and Objects/Box.java
@@ -1,4 +1,12 @@
 import java.util.*;
+/*
+Create a class Box that uses a parameterized constructor to initialize the dimensions of a box. 
+The dimensions of the Box are width, height, depth. 
+The class should have a method that can return the volume of the box. 
+Create an object of the Box class and test the functionalities.
+
+*/
+
 public class Box{
     int width;
     int depth;
